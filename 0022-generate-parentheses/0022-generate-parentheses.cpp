260bbd0f1a8a7aa13db085bfe430c8a6b33ec750
Sudarshan8125 <sudarshan08122005@gmail.com>
2026-07-05 12:41:26 +0530

    Time: 4 ms (33.37%), Space: 13 MB (83.78%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void helper(string& temp,int first,int second,vector<string>& ans){
+        if(first==0 && second==0){
+            ans.push_back(temp);
+            return;
+        }
+
+        if(second>first){
+            temp.push_back(')');
+            helper(temp,first,second-1,ans);
+            temp.pop_back();
+        }
+        
+        if(first){
+                temp.push_back('(');
+                helper(temp,first-1,second,ans);
+                temp.pop_back();
+        }
+        return;
+
+    }
+    vector<string> generateParenthesis(int n) {
+        // backtraciing type question
+
+        string temp = {};
+        int eff = 0;
+        vector<string> ans;
+        helper(temp,n,n,ans);
+        return ans;
+    }
+};
