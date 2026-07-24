58bd00b46d283d8f106d193aed00e40c89df6c4a
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-07-24 19:09:19 +0530

    Time: 33 ms (20.29%), Space: 23.7 MB (23.92%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int n,m;
-    void bfs(int i,int j,vector<vector<char>>& grid,vector<vector<int>>& vis){
+    void bfs(int i,int j,vector<vector<char>>& grid){
         queue<pair<int,int>> q;
         q.push({i,j});
-        vis[i][j] = 1;
+        grid[i][j] = '2';
         vector<pair<int,int>> dir = {{0,1},{0,-1},{1,0},{-1,0}};
         while(!q.empty()){
             auto [x,y] = q.front();
@@ -14,9 +14,9 @@ public:
                 int xn = x+a;
                 int yn = y+b;
                 if(xn>=0 && xn<n && yn>=0 && yn<m){//for a valid coordinate
-                    if(grid[xn][yn]=='1' && !vis[xn][yn]){//for unvisited land
+                    if(grid[xn][yn]=='1'){//for unvisited land
                         q.push({xn,yn});
-                        vis[xn][yn] = 1;
+                        grid[xn][yn] = '2';
                     }
                 }
             }   
@@ -25,13 +25,13 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         n = grid.size();
         m = grid[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
+
         int ans = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if (!vis[i][j] && grid[i][j] == '1') {
+                if (grid[i][j] == '1') {
                     ans++;
-                    bfs(i, j, grid, vis);
+                    bfs(i, j, grid);
                 }
             }
         }
