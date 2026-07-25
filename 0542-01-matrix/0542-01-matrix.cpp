69b619ee02b475cec4ca3461f8923db6f39731ed
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-07-25 19:17:34 +0530

    Time: 12 ms (81.51%), Space: 34.7 MB (87.15%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -5,31 +5,34 @@ public:
         int n = mat.size();
         int m = mat[0].size();
 
-        vector<vector<int>> ans(n,vector<int>(m,-1));
+        vector<vector<int>> ans(n,vector<int>(m,0));
         queue<pair<int,int>> q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(mat[i][j] == 0){
-                    q.push({i,j}); // all zeros pushed
-                    ans[i][j] = 0;
-                }
+                if(mat[i][j] == 0) q.push({i,j}); // all zeros pushed
             }
         }
 
         int dx[] = {0,0,-1,1};
         int dy[] = {-1,1,0,0};
+        int lvl = 0;
         while(!q.empty()){
-            auto [r,c] = q.front();
-            q.pop();
+            int size = q.size();
+            lvl++;
+            for(int c=0;c<size;c++){ //pop out an entire level
+                auto [a,b] = q.front();
+                q.pop();
 
-            for(int i=0;i<4;i++){
-                int xn = r + dx[i];
-                int yn = c + dy[i];
+                for(int i=0;i<4;i++){
+                    int xn = a + dx[i];
+                    int yn = b + dy[i];
 
-                if(xn>=0 && xn<n && yn>=0 && yn<m){
-                    if(ans[xn][yn] == -1){
-                        ans[xn][yn] = ans[r][c] + 1; // Distance is parent distance + 1
-                        q.push({xn, yn});           // Push to queue to explore its neighbors later
+                    if(xn>=0 && xn<n && yn>=0 && yn<m){
+                        if(mat[xn][yn] == 1){
+                            mat[xn][yn] = 0; //decays to 0 or gets rotten
+                            q.push({xn,yn});
+                            ans[xn][yn] = lvl;
+                        }
                     }
                 }
             }
