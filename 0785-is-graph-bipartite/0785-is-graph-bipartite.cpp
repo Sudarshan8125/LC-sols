2c188cd4fc6f505dbe372c38217a97f1d680dc32
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-07-26 19:05:06 +0530

    Time: 0 ms (100%), Space: 17.2 MB (93.04%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,33 +1,26 @@
 class Solution {
 private:
-    bool canColor(int src,vector<int>& vis,vector<vector<int>>& graph){  //detect fallacy
-        vis[src] = 0;
-        queue<int> q;
-        q.push(src);
+    bool dfs(int src,int col,vector<int>& vis,vector<vector<int>>& graph){
+        vis[src] = col;
 
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-
-            for(auto it:graph[node]){ //check neighbouring
-                if(vis[it]==-1){
-                    q.push(it);
-                    vis[it] = !vis[node];
-                }
-                else if(vis[it] == vis[node]) return false; //neighbour having the same color
+        for(auto it:graph[src]){ //for adj nodes
+            if(vis[it]==-1){
+                if(dfs(it,!col,vis,graph)==false) return false;
             }
+            else if(vis[it] == vis[src]) return false;
         }
         return true;
     }
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        //ezy
-        int V = graph.size(); // no.of nodes
+        //using dfs
+        int V = graph.size();
         vector<int> vis(V,-1);
-        
+
         for(int i=0;i<V;i++){
-            if(vis[i]==-1)
-                if(canColor(i,vis,graph) == false) return false;        
+            if(vis[i]==-1){
+                if(dfs(i,0,vis,graph)==false) return false;
+            }
         }
         return true;
     }
