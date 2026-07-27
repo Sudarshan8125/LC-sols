665fb5c1d42aa1a98dcaa5fa0092faa929bfa48c
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-07-27 10:35:13 +0530

    Time: 3 ms (70.48%), Space: 18.4 MB (56.52%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> adj[numCourses];
+        vector<int> indeg(numCourses,0);
+        for(auto it:prerequisites){
+            int u = it[0];
+            int v = it[1];
+
+            adj[v].push_back(u);
+            indeg[u]++;
+        }
+    
+        queue<int> q;
+        for(int i=0;i<numCourses;i++){
+            if(indeg[i]==0) q.push(i); 
+        }
+
+        vector<int> ans;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for(auto it:adj[node]){
+                indeg[it]--;
+                if(indeg[it]==0) q.push(it);
+            }
+        }
+
+        return ans.size() == numCourses ? ans:vector<int>{};
+    }
+};
