4b59fd7ea93fc139f306ed637bc5611076d92969
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-07-29 00:42:55 +0530

    Time: 43 ms (86.55%), Space: 24.3 MB (91.89%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,38 +1,46 @@
-class Solution {
-public:
-    int minimumEffortPath(vector<vector<int>>& heights) {
-        int r = heights.size();
-        int c = heights[0].size();
-
-        // define edge weight as the diff bw val 
-        vector<vector<int>> effort(r,vector<int>(c,INT_MAX));
-
-        set<pair<int,pair<int,int>>> st;
-        st.insert({0,{0,0}});
-        effort[0][0] = 0;
-
-        int dx[] = {-1,1,0,0};
-        int dy[] = {0,0,-1,1};
-        while(!st.empty()){
-            auto [eff,coord] = *(st.begin());
-            auto [x,y] = coord;
-            st.erase(st.begin());
-            if(x == r-1 && y== c-1) return effort[r-1][c-1];
-            for(int i=0;i<4;i++){
-                int xn = x + dx[i];
-                int yn = y + dy[i];
-
-                if(xn>=0 && xn<r && yn>=0 && yn<c){
-                    int eff = abs(heights[x][y] - heights[xn][yn]);
-                    eff = max(effort[x][y],eff); //bec that the real eff
-                    if(effort[xn][yn]>eff){
-                        if(effort[xn][yn] !=INT_MAX) st.erase({effort[xn][yn],{xn,yn}});
-                        effort[xn][yn] = eff;
-                        st.insert({effort[xn][yn],{xn,yn}});
-                    }
-                }
-            }
-        }
-        return effort[r-1][c-1];
-    }
+class Solution { 
+public: 
+    int minimumEffortPath(vector<vector<int>>& heights) { 
+        int r = heights.size(); 
+        int c = heights[0].size(); 
+        
+        vector<vector<int>> effort(r, vector<int>(c, INT_MAX)); 
+        // Min-heap storing: {effort, {x, y}}
+        priority_queue<pair<int, pair<int, int>>, 
+                       vector<pair<int, pair<int, int>>>, 
+                       greater<pair<int, pair<int, int>>>> pq; 
+        
+        pq.push({0, {0, 0}}); 
+        effort[0][0] = 0; 
+        
+        int dx[] = {-1, 1, 0, 0}; 
+        int dy[] = {0, 0, -1, 1}; 
+        
+        while(!pq.empty()){ 
+            auto [eff, coord] = pq.top(); 
+            auto [x, y] = coord; 
+            pq.pop(); 
+            
+            // Early exit as soon as we reach the destination
+            if(x == r - 1 && y == c - 1) return eff; 
+            
+            // Skip outdated pairs with higher effort
+            if(eff > effort[x][y]) continue; 
+            
+            for(int i = 0; i < 4; i++){ 
+                int xn = x + dx[i]; 
+                int yn = y + dy[i]; 
+                
+                if(xn >= 0 && xn < r && yn >= 0 && yn < c){ 
+                    int next_eff = max(eff, abs(heights[x][y] - heights[xn][yn])); 
+                    
+                    if(effort[xn][yn] > next_eff){ 
+                        effort[xn][yn] = next_eff; 
+                        pq.push({next_eff, {xn, yn}}); 
+                    } 
+                } 
+            } 
+        } 
+        return 0; 
+    } 
 };
