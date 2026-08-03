ccb9383138d38c00f14e6588ad9ba17d2145947a
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-08-03 19:18:40 +0530

    Time: 0 ms (100%), Space: 10.7 MB (47.36%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-
-    int f(int idx,vector<int>& nums,vector<int>& dp){
-        if(idx<0) return 0;
-        if(dp[idx]!=-1) return dp[idx];
-
-        //pick
-        int pick = f(idx-2,nums,dp) + nums[idx];
-        //not 
-        int notpick = f(idx-1,nums,dp);
-
-        return dp[idx] = max(pick,notpick);
-    }
     int rob(vector<int>& nums) {
-        //using memoised sol
         int n = nums.size();
-        vector<int> dp(n,-1);
-        return f(n-1,nums,dp);
+        vector<int> dp(n,0);
+
+        dp[0] = nums[0];
+    
+        for(int i=1;i<n;i++){
+
+            int take = nums[i];
+            if(i>1) take+=dp[i-2];
+            int nottake = dp[i-1];
+
+            dp[i] = max(take,nottake);
+        }
+        return dp[n-1];
     }
 };
