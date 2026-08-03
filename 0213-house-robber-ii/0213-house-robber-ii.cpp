f03fdd494233ce53af9d8bb01098766d5a351da5
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-08-03 20:08:18 +0530

    Time: 0 ms (100%), Space: 10.7 MB (59.49%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
-    int f(int idx,vector<int>& nums,vector<int>& dp,bool flag){
+    int f(int idx,int start,vector<int>& nums,vector<int>& dp){
 
-        if(idx<0) return 0;
-        if(idx == 0){
-            if(flag==1) return nums[0];
-            else return 0;
-        }
+        if(idx<start) return 0;
 
         if(dp[idx]!=-1) return dp[idx];
 
 
-        int take = f(idx-2,nums,dp,flag) + nums[idx];
-        int nottake = f(idx-1,nums,dp,flag);
+        int take = f(idx-2,start,nums,dp) + nums[idx];
+        int nottake = f(idx-1,start,nums,dp);
 
         return dp[idx] = max(take,nottake);
     }
@@ -24,7 +20,7 @@ public:
         vector<int> dp1(n, -1);
         vector<int> dp2(n, -1);
 
-        bool flag = 1;
-        return max(f(n-2,nums,dp1,flag) , f(n-1,nums,dp2,!flag));
+
+        return max(f(n-2,0,nums,dp1) , f(n-1,1,nums,dp2));
     }
 };
