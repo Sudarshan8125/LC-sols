d17d8ead2b1f89564873b64aa8fbf3109671a5e4
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-08-04 18:02:26 +0530

    Time: 0 ms (100%), Space: 9.5 MB (5.35%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -16,9 +16,22 @@ public:
     }
 
     int uniquePaths(int m, int n) {
-        //count all possible ways that lead to n-1,m-1
 
-        vector<vector<int>> dp(n,vector(m,-1));
-        return f(n-1,m-1,dp);    
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+
+        dp[0][0] = 1; //from start to start only one way if it was the case
+         
+        for(int r=0;r<m;r++){
+            for(int c=0;c<n;c++){
+                if(r==0 && c==0) continue;
+                int up = 0,left = 0;
+                if(r>0) up = dp[r-1][c];
+                if(c>0) left = dp[r][c-1];
+
+                dp[r][c] = up+left;
+            }
+        }
+        //now tabulate this sol  
+        return dp[m-1][n-1];
     }
 };
