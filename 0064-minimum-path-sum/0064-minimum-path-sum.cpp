76102a834652add0a55b923cc9ad0503e33c1d68
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-08-04 20:54:41 +0530

    Time: 0 ms (100%), Space: 16.4 MB (16.97%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -5,16 +5,34 @@ public:
         if(r==0 && c==0) return grid[0][0];
         if(dp[r][c]!=-1) return dp[r][c];
         //do all stuffs
-        int up = f(r-1,c,grid,dp);
-        int left = f(r,c-1,grid,dp);
+        int up = grid[r][c] + f(r-1,c,grid,dp);
+        int left = grid[r][c] + f(r,c-1,grid,dp);
 
-        return dp[r][c] = grid[r][c] + min(up,left);
+        return dp[r][c] = min(up,left);
     }
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<int>> dp(n,vector<int>(m,-1));
+        //conversion to tabulation
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0){
+                    dp[i][j] = grid[i][j];
+                    continue;
+                }
 
-        return f(n-1,m-1,grid,dp);
+                int up = INT_MAX;
+                int left = INT_MAX;
+
+                if(i>0) up = grid[i][j] + dp[i-1][j];
+                if(j>0) left = grid[i][j] + dp[i][j-1];
+
+                dp[i][j] = min(up,left);
+            }
+        }
+
+        return dp[n-1][m-1];
     }
 };
