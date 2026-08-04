ad7f6b0fa0d07079dc967261c275eb831ca27da9
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-08-04 19:44:24 +0530

    Time: 0 ms (100%), Space: 11.7 MB (72.79%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -20,26 +20,29 @@ public:
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
 
-        vector<vector<int>> dp(m,vector<int>(n,-1));
+        vector<int> prev(n,0);
 
         //convert to tabultion
+        //optimizing space
         for(int r=0;r<m;r++){
+            vector<int> temp(n,0);
             for(int c=0;c<n;c++){
                 if(obstacleGrid[r][c]){
-                    dp[r][c] = 0;
+                    temp[c] = 0;
                     continue;
                 }
                 if(r==0 && c==0){
-                    dp[0][0] = 1;
+                    temp[0] = 1;
                     continue;
                 }
                 int up = 0, left = 0;
-                if(r>0) up = dp[r-1][c];
-                if(c>0) left = dp[r][c-1];
+                if(r>0) up = prev[c];
+                if(c>0) left = temp[c-1];
 
-                dp[r][c] = up+left;
+                temp[c] = up+left;
             }
+            prev = temp;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
