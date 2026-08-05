0d5640d1a4c900a174979080a171c04d17c9c6f8
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-08-05 22:10:17 +0530

    Time: 66 ms (26.42%), Space: 21 MB (55.63%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-
     int f(int r,int c1,int c2,vector<vector<int>>& grid,vector<vector<vector<int>>>& dp){
         int n = grid.size();
         int m = grid[0].size();
@@ -27,6 +26,40 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<vector<int>>> dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
-        return f(0,0,m-1,grid,dp);
+
+        //declare base cases
+        for(int c1=0;c1<m;c1++){
+            for(int c2=0;c2<m;c2++){
+                if(c1==c2) dp[n-1][c1][c2] = grid[n-1][c1];
+                else dp[n-1][c1][c2] = grid[n-1][c1] + grid[n-1][c2];
+            }
+        }
+        //loops
+        for(int r=n-2;r>=0;r--){
+            for(int c1=0;c1<m;c1++){ //these 2 make cases for n-2th row both at 00 11 01 etc
+                for(int c2=0;c2<m;c2++){
+
+                    int maxi = INT_MIN; //har n-2th row ke case ke liye // explore nichli row wale options
+                    for(int i=-1; i<=1; i++){
+                        for(int j=-1; j<=1; j++){
+                            int value = 0;
+                            if(c1==c2) value = grid[r][c1];
+                            else value = grid[r][c1] + grid[r][c2];
+                            if(c1 + i >= 0 && c1 + i < m && c2 + j >= 0 && c2 + j < m) {
+                                value += dp[r+1][c1+i][c2+j];
+                            } else {
+                                value += -1e9; // Fixed: Add penalty for illegal out-of-bounds moves
+                            }
+                            maxi = max(maxi,value);
+                        }
+                    }
+                    
+                    dp[r][c1][c2] = maxi;  //assign the max poss value for that case
+                }
+            }
+        }
+
+
+        return dp[0][0][m-1];
     }
 };
