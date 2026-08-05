8df152ce564c56e87725c175d205a0102c750517
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-08-05 15:42:16 +0530

    Time: 1 ms (49.16%), Space: 15 MB (47.34%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -19,8 +19,28 @@ public:
         int n = matrix.size();
         int mini = INT_MAX;
         //case of overlapping subproblem
+        //tabulation approach
         vector<vector<int>> dp(n, vector<int>(n, -1e9));
-        for(int i=n-1;i>=0;i--) mini = min(mini,f(n-1,i,matrix,dp)); //row,col
+
+        //write the base case
+        for(int j=0;j<n;j++) dp[0][j] = matrix[0][j];
+        //rec n-1->0 thefo tab 0->n-1
+        for(int i=1;i<n;i++){
+            for(int j=0;j<n;j++){
+                
+                int up = dp[i-1][j];
+                int left = INT_MAX,right = INT_MAX;
+                if(j>=1) left = dp[i-1][j-1];
+                if(j+1<n) right = dp[i-1][j+1];
+
+                dp[i][j] = matrix[i][j] + min({up,left,right});
+            }
+        }
+        
+        for(int j=0;j<n;j++){ //check for the min in last row
+            mini = min(mini,dp[n-1][j]);
+        }
+
         return mini;
     }
 };
