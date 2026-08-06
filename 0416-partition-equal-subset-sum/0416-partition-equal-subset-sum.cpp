1aaf87b8203a406dbe7f1a07a6465f7e09d3d0e2
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-08-06 19:52:00 +0530

    Time: 107 ms (69.84%), Space: 110.4 MB (45.61%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -18,11 +18,25 @@ public:
     }
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
-        //for partitioning find subset with sum = arrsum/2;
         int sum = accumulate(nums.begin(),nums.end(),0);
         if(sum%2 == 1) return false;
         
-        vector<vector<int>> dp(n,vector<int>(sum/2+1,-1));
-        return f(n-1,sum/2,nums,dp); 
+        vector<vector<int>> dp(n,vector<int>(sum/2+1,false));
+
+        //tabulation approach
+        for(int i=0;i<n;i++) dp[i][0] = true;
+        if(nums[0] <= sum/2) dp[0][nums[0]] = true; //bound safe check behind // 0th row done
+
+        for(int idx = 1; idx<n; idx++){
+            for(int target = 1; target <= sum/2 ;target++){
+                bool nottake = dp[idx-1][target];
+                bool take = false;
+                if(target >= nums[idx]) take = dp[idx-1][target-nums[idx]];
+
+                dp[idx][target] = nottake || take; //can we reach the val of sum/2 in the range idx->0.
+            }
+        }
+
+        return dp[n-1][sum/2]; 
     }
 };
