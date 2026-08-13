49992143e4a3705d70671f3b03d56bce8d1682c9
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-08-14 02:50:51 +0530

    Time: 27 ms (94.12%), Space: 13.3 MB (97.19%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -4,12 +4,13 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
+        vector<vector<int>> prev(m,vector<int>(m,0));
+        vector<vector<int>> curr(m,vector<int>(m,0));
 
         for(int c1=0;c1<m;c1++){
             for(int c2=0;c2<m;c2++){
-                if(c1!=c2) dp[n-1][c1][c2] = grid[n-1][c1] + grid[n-1][c2];
-                else dp[n-1][c1][c2] = grid[n-1][c1];
+                if(c1!=c2) prev[c1][c2] = grid[n-1][c1] + grid[n-1][c2];
+                else prev[c1][c2] = grid[n-1][c1];
             }
         }
 
@@ -24,15 +25,16 @@ public:
                             int x = c1+i;
                             int y = c2+j;
                             if(x >= 0 && x < m && y >= 0 && y < m){ 
-                                maxi = max(maxi, dp[r+1][x][y]);
+                                maxi = max(maxi, prev[x][y]);
                             }
                         }  
                     }            
                     int cherries = (c1 == c2) ? grid[r][c1] : grid[r][c1] + grid[r][c2];     
-                    dp[r][c1][c2] = cherries + maxi;               
+                    curr[c1][c2] = cherries + maxi;               
                 }
             }
+            prev = curr;
         }
-        return dp[0][0][m-1]; //passed up the fixed start points
+        return prev[0][m-1]; //passed up the fixed start points
     }
 };
