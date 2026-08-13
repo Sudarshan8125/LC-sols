3d7fe9718f1e432ab5e22e151dbb81a0fdbd5b4b
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-08-13 21:56:21 +0530

    Time: 3 ms (30.41%), Space: 16.6 MB (16.77%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,42 +1,22 @@
 class Solution {
 public:
     int f(int r,int c,vector<vector<int>>& grid,vector<vector<int>>& dp){
-        if(r<0 || c<0) return INT_MAX;
+        if(r<0 || c<0) return 1e9;
         if(r==0 && c==0) return grid[0][0];
-        if(dp[r][c]!=-1) return dp[r][c];
-        //do all stuffs
-        int up = f(r-1,c,grid,dp);
-        int left = f(r,c-1,grid,dp);
 
-        return dp[r][c] = grid[r][c] + min(up,left);
+        if(dp[r][c]!=-1) return dp[r][c];
+        //exp all possible options
+        int up = grid[r][c] + f(r-1,c,grid,dp);
+        int left = grid[r][c] + f(r,c-1,grid,dp);
+
+        //min of all op would be the answer
+        return dp[r][c] = min(up,left);
     }
     int minPathSum(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        
-        //conversion to tabulation
-        //space opti
-        vector<int> prev(m,0);
+        int m = grid.size();
+        int n = grid[0].size();
 
-        for(int i=0;i<n;i++){
-            vector<int> temp(m,0);
-            for(int j=0;j<m;j++){
-                if(i==0 && j==0){
-                    temp[j] = grid[i][j];
-                    continue;
-                }
-
-                int up = INT_MAX;
-                int left = INT_MAX;
-
-                if(i>0) up = grid[i][j] + prev[j];
-                if(j>0) left = grid[i][j] + temp[j-1];
-
-                temp[j] = min(up,left); //no return cause we storing states
-            }
-            prev = temp;
-        }
-
-        return prev[m-1];
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return f(m-1,n-1,grid,dp);
     }
 };
