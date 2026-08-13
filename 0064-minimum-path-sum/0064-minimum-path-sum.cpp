8176e5b97582ef2fdafa2bf11b1941c2ed2fd7f7
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-08-13 22:05:40 +0530

    Time: 0 ms (100%), Space: 15.4 MB (90.96%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,44 +1,31 @@
 class Solution {
 public:
-    int f(int r, int c, vector<vector<int>>& grid, vector<vector<int>>& dp) {
-        if (r < 0 || c < 0)
-            return 1e9;
-        if (r == 0 && c == 0)
-            return grid[0][0];
-
-        if (dp[r][c] != -1)
-            return dp[r][c];
-        // exp all possible options
-        int up = grid[r][c] + f(r - 1, c, grid, dp);
-        int left = grid[r][c] + f(r, c - 1, grid, dp);
-
-        // min of all op would be the answer
-        return dp[r][c] = min(up, left);
-    }
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
 
-        // convert to tabulation
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+        //space opti
+        vector<int> prev(n, 0);
+        vector<int> curr(n, 0);
 
         for (int r = 0; r < m; r++) {
             for (int c = 0; c < n; c++) {
                 if (r == 0 && c == 0) {
-                    dp[r][c] = grid[r][c];
+                    curr[c] = grid[r][c];
                     continue;
                 }
 
                 int up = 1e9;
                 int left = 1e9;
-                if(r>0) up = grid[r][c] + f(r - 1, c, grid, dp);
-                if(c>0) left = grid[r][c] + f(r, c - 1, grid, dp);
+                if(r>0) up = grid[r][c] + prev[c];
+                if(c>0) left = grid[r][c] + curr[c - 1];
 
                 // min of all op would be the answer
-                dp[r][c] = min(up, left);
+                curr[c] = min(up, left);
             }
+            prev = curr;
         }
 
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
