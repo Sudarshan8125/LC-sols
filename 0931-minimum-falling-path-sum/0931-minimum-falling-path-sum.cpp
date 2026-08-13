0d03b2e29b0ae26daee5b442e64caa7ed5d38d04
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-08-13 23:34:12 +0530

    Time: 0 ms (100%), Space: 16.1 MB (11.37%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,48 +1,28 @@
 class Solution {
 public:
+    int f(int r,int c,vector<vector<int>>& mat,vector<vector<int>>& dp){
+        int n = mat.size();
+        if(c<0 || c>=n) return 1e9;
+        if(r==0) return mat[0][c];
 
-    int f(int row,int col,vector<vector<int>>& matrix,vector<vector<int>>& dp){
-        //base cases
-        int n = matrix.size();
-        if (col < 0 || col >= n) return 1e9; 
-        if(row == 0) return matrix[row][col];
+        if(dp[r][c]!=-1e9) return dp[r][c];
 
-        if (dp[row][col] != -1e9) return dp[row][col];
-        //do all stuffs
-        int up = f(row-1,col,matrix,dp);
-        int left = f(row-1,col-1,matrix,dp);
-        int right = f(row-1,col+1,matrix,dp);
+        int up = f(r-1,c,mat,dp);
+        int left = f(r-1,c-1,mat,dp);
+        int right = f(r-1,c+1,mat,dp);
 
-        return dp[row][col] = matrix[row][col] + min({up,left,right});
+        return dp[r][c] = mat[r][c] + min({up,right,left});
+
     }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        int mini = INT_MAX;
+        vector<vector<int>> dp(n,vector<int>(n,-1e9)); // keep a val that doesnt gets mixed
 
-        //space opti
-        vector<int> prev(n,0);
-
-        //write the base case
-        for(int j=0;j<n;j++) prev[j] = matrix[0][j];
-
-        for(int i=1;i<n;i++){
-            vector<int> temp(n,0);
-            for(int j=0;j<n;j++){
-                
-                int up = prev[j];
-                int left = INT_MAX,right = INT_MAX;
-                if(j>=1) left = prev[j-1];
-                if(j+1<n) right = prev[j+1];
-
-                temp[j] = matrix[i][j] + min({up,left,right});
-            }
-            prev = temp;
+        int mini = 1e9;
+        for(int i=n-1;i>=0;i--){ //for the entire last row
+            int s = f(n-1,i,matrix,dp);
+            mini = min(mini,s);
         }
-        
-        for(int j=0;j<n;j++){ //check for the min in last row
-            mini = min(mini,prev[j]);
-        }
-
         return mini;
     }
 };
