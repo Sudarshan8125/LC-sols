c4fdfdf51d907d5d5b45570becb3bd78fcf830e2
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-08-13 23:41:54 +0530

    Time: 3 ms (39.07%), Space: 15 MB (67.53%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    int f(int r,int c,vector<vector<int>>& mat,vector<vector<int>>& dp){
-        int n = mat.size();
-        if(c<0 || c>=n) return 1e9;
-        if(r==0) return mat[0][c];
-
-        if(dp[r][c]!=-1e9) return dp[r][c];
-
-        int up = f(r-1,c,mat,dp);
-        int left = f(r-1,c-1,mat,dp);
-        int right = f(r-1,c+1,mat,dp);
-
-        return dp[r][c] = mat[r][c] + min({up,right,left});
-
-    }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
         vector<vector<int>> dp(n,vector<int>(n,-1e9)); // keep a val that doesnt gets mixed
 
+        for(int c=0;c<n;c++){
+            dp[0][c] = matrix[0][c];
+        }
+
+        for(int r=1;r<n;r++){
+            for(int c=0;c<n;c++){
+
+                int left = 1e9, right = 1e9;
+
+                int up = dp[r-1][c];
+                if(c>0) left = dp[r-1][c-1];
+                if(c<n-1) right = dp[r-1][c+1];
+                dp[r][c] = matrix[r][c] + min({up,right,left});                
+            }
+        }
+
         int mini = 1e9;
-        for(int i=n-1;i>=0;i--){ //for the entire last row
-            int s = f(n-1,i,matrix,dp);
+        for(int c=n-1;c>=0;c--){ //for the entire last row
+            int s = dp[n-1][c];
             mini = min(mini,s);
         }
         return mini;
