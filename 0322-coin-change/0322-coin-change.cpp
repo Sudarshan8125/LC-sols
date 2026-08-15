8733da201ed96f6bd5be4f3f3adfef23fd4113a9
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-08-16 02:36:22 +0530

    Time: 59 ms (14.92%), Space: 49.7 MB (23.81%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    int f(int idx,int amount,vector<int>&coins,vector<vector<int>>& dp){
+    int f(int idx,int amount,vector<int>& coins,vector<vector<int>>& dp){
+        if(amount == 0) return 0; //to make amount 0 =>min length we pick is 0.
+        if(idx<0) return 1e9;
 
-        if(amount == 0) return 0;//short circuit
-        if(idx == -1) return 1e9; //to discard all the cases which cant make amount 0 till
+        if(dp[idx][amount]!=-1) return dp[idx][amount];
+        //pick
+        int pick = 1e9;
+        if(amount>=coins[idx])
+            pick = 1 + f(idx,amount-coins[idx],coins,dp);
 
-        if(dp[idx][amount]!=0) return dp[idx][amount];
-        //not take
-        int nottake = f(idx-1,amount,coins,dp);
+        //not pick
+        int notpick = f(idx-1,amount,coins,dp);
 
-        //take
-        int take = 1e9;
-        if(amount >= coins[idx])
-            take = 1+f(idx,amount-coins[idx],coins,dp);
-
-        return dp[idx][amount] = min(take,nottake);
+        return dp[idx][amount] = min(pick,notpick);
     }
-
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1)); //size cant ever be -1.
         int ans = f(n-1,amount,coins,dp);
-        if(ans>=1e9) return -1;
-        else return ans;
-        //f will ret the min coins req to make up the amount specified
+        return ans>=1e9 ? -1:ans;
     }
 };
