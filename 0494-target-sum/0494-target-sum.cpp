a9fd072c11d76fd6c945d2cf7c976eeeb1b13260
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-08-16 23:57:04 +0530

    Time: 0 ms (100%), Space: 12.6 MB (64.62%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,29 +1,46 @@
 class Solution {
-public:
-    int f(int idx,int offset,int target,int sum,vector<int>& nums,vector<vector<int>>& dp){
-        if(idx==0){
-            if (nums[idx] == 0 && sum == target) return 2;
-            if(sum == target-nums[idx] || sum == target+nums[idx]) return 1;
-            return 0;
-        }
-
-        if(dp[idx][sum + offset]!=-1e9) return dp[idx][sum+offset];
-        //nottake
-        int p = f(idx-1,offset,target,sum + nums[idx],nums,dp);
-
-        //take
-        int n = f(idx-1,offset,target,sum - nums[idx],nums,dp);
-
-        return dp[idx][offset+sum] = p+n;
-    }
+public:    
+    // int f(int idx,int tar,vector<int>& arr,vector<vector<int>>& dp){
+        
+    //     if(idx == 0){
+    //         if(tar == 0 && arr[0] == 0) return 2;
+    //         if(tar == 0) return 1;
+    //         if(arr[0] == tar) return 1;
+    //         return 0;
+    //     }
+        
+    //     if(dp[idx][tar] != -1) return dp[idx][tar];
+        
+    //     int nottake = f(idx-1,tar,arr,dp);
+    //     int take = 0;
+    //         if(tar >= arr[idx]) take = f(idx-1,tar-arr[idx],arr,dp);
+            
+    //     return dp[idx][tar] = take + nottake;
+    // }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        //need to try alll ways 
-        //pick=> +
-        //notpick=> -
         int tsum = accumulate(nums.begin(),nums.end(),0);
+        if((tsum-target)%2 != 0 || tsum-target<0) return 0;
 
-        vector<vector<int>> dp(n,vector<int>(2*tsum+1,-1e9));
-        return f(n-1,tsum,target,0,nums,dp);
+        int sum = (tsum-target)/2;
+        vector<int> prev(sum+1,0);
+
+        if(nums[0] == 0){
+            prev[0] = 2; //both take and nottake can be executed
+        }
+        else prev[0] = 1; //only not take can be executed
+        if(nums[0]!=0 && nums[0]<=sum) prev[nums[0]] = 1;//take case for above //if in bound
+
+
+        for(int idx=1;idx<n;idx++){
+            for(int tar=sum;tar>=0;tar--){
+                int nottake = prev[tar];
+                int take = 0;
+                if(tar >= nums[idx]) take = prev[tar-nums[idx]];
+            
+                prev[tar] = take + nottake;
+            }
+        }
+        return prev[sum];
     }
 };
