9984067a3be9f78432d061497061ee93b70a84fc
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-08-17 17:04:24 +0530

    Time: 11 ms (43.98%), Space: 17.9 MB (52.51%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,42 +1,28 @@
 class Solution {
 public:
-    // int f(int idx, int buy, vector<int>& prices, int n,int txend,vector<vector<int>>& dp) {
-    //     if (idx == n || buy == txend) {
-    //         return 0;
-    //     }
+    int f(int idx, int buy, vector<int>& prices, int n,int txend,vector<vector<int>>& dp) {
+        if (idx == n || buy == txend) {
+            return 0;
+        }
 
-    //     if (dp[idx][buy] != -1)
-    //         return dp[idx][buy];
+        if (dp[idx][buy] != -1)
+            return dp[idx][buy];
 
-    //     if (buy % 2 == 0) {
-    //         int b = -prices[idx] + f(idx + 1, buy + 1, prices, n, txend, dp);
-    //         int nb = f(idx + 1, buy, prices, n, txend, dp);
-    //         return dp[idx][buy] = max(b, nb);
-    //     } else {
-    //         int s = +prices[idx] + f(idx + 1, buy + 1, prices, n, txend, dp);
-    //         int ns = f(idx + 1, buy, prices, n, txend, dp);
-    //         return dp[idx][buy] = max(s, ns);
-    //     }
-    // }
+        if (buy % 2 == 0) {
+            int b = -prices[idx] + f(idx + 1, buy + 1, prices, n, txend, dp);
+            int nb = f(idx + 1, buy, prices, n, txend, dp);
+            return dp[idx][buy] = max(b, nb);
+        } else {
+            int s = +prices[idx] + f(idx + 1, buy + 1, prices, n, txend, dp);
+            int ns = f(idx + 1, buy, prices, n, txend, dp);
+            return dp[idx][buy] = max(s, ns);
+        }
+    }
     int maxProfit(int k, vector<int>& prices) {
          //try out all ways
         int n = prices.size();
 
-        vector<vector<int>> dp(n+1,vector<int>(2*k+1,0)); 
-
-        for (int idx = n - 1; idx >= 0; idx--) {
-            for (int buy = 2*k-1; buy >= 0; buy--) {
-                if (buy % 2 == 0) {
-                    int b = -prices[idx] + dp[idx + 1][buy + 1];
-                    int nb = dp[idx + 1][buy];
-                    dp[idx][buy] = max(b, nb);
-                } else {
-                    int s = +prices[idx] + dp[idx + 1][buy + 1];
-                    int ns = dp[idx + 1][buy];
-                    dp[idx][buy] = max(s, ns);
-                }
-            }
-        }
-        return dp[0][0]; // with buy permission
+        vector<vector<int>> dp(n+1,vector<int>(2*k+1,-1)); 
+        return f(0,0,prices,n,2*k,dp); 
     }
 };
