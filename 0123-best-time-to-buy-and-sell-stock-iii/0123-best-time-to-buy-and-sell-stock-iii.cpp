ba4e4d020a42b54e6d6ec5b357d6ad86beb9dbc7
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-08-17 16:44:30 +0530

    Time: 130 ms (62.3%), Space: 139.6 MB (58.99%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,30 +1,43 @@
 class Solution {
 public:
-    int f(int idx,int buy,vector<int>& prices,int n,vector<vector<int>>& dp){
-        if(idx==n || buy==0){
-            return 0;
-        }
+//     int f(int idx, int buy, vector<int>& prices, int n,
+//           vector<vector<int>>& dp) {
+//         if (idx == n || buy == 4) {
+//             return 0;
+//         }
 
-        if(dp[idx][buy]!=-1) return dp[idx][buy];
-        int profit = 0;
-        if(buy%2==0){
-            int b = -prices[idx] + f(idx+1,buy-1,prices,n,dp);
-            int nb = f(idx+1,buy,prices,n,dp);
-            profit = max(b,nb);
-        }
-        else{
-            int s = +prices[idx] + f(idx+1,buy-1,prices,n,dp);
-            int ns = f(idx+1,buy,prices,n,dp);
-            profit = max(s,ns);
-        }
-        return dp[idx][buy] = profit;
-    }
+//         if (dp[idx][buy] != -1)
+//             return dp[idx][buy];
+
+//         if (buy % 2 == 0) {
+//             int b = -prices[idx] + f(idx + 1, buy + 1, prices, n, dp);
+//             int nb = f(idx + 1, buy, prices, n, dp);
+//             return dp[idx][buy] = max(b, nb);
+//         } else {
+//             int s = +prices[idx] + f(idx + 1, buy + 1, prices, n, dp);
+//             int ns = f(idx + 1, buy, prices, n, dp);
+//             return dp[idx][buy] = max(s, ns);
+//         }
+//     }
     int maxProfit(vector<int>& prices) {
-        //try out all ways
+        // try out all ways
         int n = prices.size();
-        //even b buy
-        //odd b sell
-        vector<vector<int>> dp(n,vector<int>(5,-1)); //up until b==4
-        return f(0,4,prices,n,dp); //with buy permission
+        // even b buy
+        // odd b sell
+        vector<vector<int>> dp(n + 1, vector<int>(5, 0));
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int buy = 3; buy >= 0; buy--) {
+                if (buy % 2 == 0) {
+                    int b = -prices[idx] + dp[idx + 1][buy + 1];
+                    int nb = dp[idx + 1][buy];
+                    dp[idx][buy] = max(b, nb);
+                } else {
+                    int s = +prices[idx] + dp[idx + 1][buy + 1];
+                    int ns = dp[idx + 1][buy];
+                    dp[idx][buy] = max(s, ns);
+                }
+            }
+        }
+        return dp[0][0]; // with buy permission
     }
 };
