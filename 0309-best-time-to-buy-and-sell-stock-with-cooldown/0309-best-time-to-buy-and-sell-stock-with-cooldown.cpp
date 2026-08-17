2b1686f5f4fcbd79f03ab8e2c2cac2a70eecb437
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-08-17 19:34:21 +0530

    Time: 2 ms (39.26%), Space: 16.2 MB (36.56%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    // int f(int idx, int buy, vector<int>& prices, vector<vector<int>>& dp,
+    //       int n) {
+    //     if (idx >= n)
+    //         return 0;
+
+    //     if (dp[idx][buy] != -1)
+    //         return dp[idx][buy];
+    //     int profit = 0;
+    //     if (buy) {
+    //         int b = -prices[idx] + f(idx + 1, 0, prices, dp, n);
+    //         int nb = f(idx + 1, 1, prices, dp, n);
+    //         profit = max(b, nb);
+    //     } else {
+    //         int s = +prices[idx] + f(idx + 2, 1, prices, dp, n);
+    //         int ns = f(idx + 1, 0, prices, dp, n);
+    //         profit = max(s, ns);
+    //     }
+    //     return dp[idx][buy] = profit;
+    // }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n + 2, vector<int>(2, 0));
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                int profit = 0;
+                if (buy) {
+                    int b = -prices[idx] + dp[idx + 1][0];
+                    int nb = dp[idx + 1][1];
+                    profit = max(b, nb);
+                } else {
+                    int s = +prices[idx] + dp[idx + 2][1];
+                    int ns = dp[idx + 1][0];
+                    profit = max(s, ns);
+                }
+                dp[idx][buy] = profit;
+            }
+        }
+        return dp[0][1];
+    }
+};
