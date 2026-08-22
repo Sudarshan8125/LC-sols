af4b36d2d593fe0dc6d685f1d328e3ec4a1778e2
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-08-22 20:07:40 +0530

    Time: 10 ms (34.1%), Space: 10.5 MB (90.42%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -19,29 +19,29 @@ public:
         int n = word1.size();
         int m = word2.size();
 
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        vector<int> prev(m+1,0);
+        vector<int> curr(m+1,0);
         //use offset +1 evry where => do 1 based indexing for dp table
-        for(int i = 0; i <= n; i++){
-            dp[i][0] = i;
-        }
-    
+        
         for(int j = 0; j <= m; j++){
-            dp[0][j] = j;
+            prev[j] = j;
         }
 
         for(int i1=1; i1<=n; i1++){
+            curr[0] = i1; 
             for(int i2=1; i2<=m; i2++){
-                if(word1[i1-1] == word2[i2-1]) dp[i1][i2] = dp[i1-1][i2-1];
+                if(word1[i1-1] == word2[i2-1]) curr[i2] = prev[i2-1];
 
                 else{
-                    int add = 1 + dp[i1][i2-1];
-                    int rem = 1 + dp[i1-1][i2];   //word1 ka part remove
-                    int rep = 1 + dp[i1-1][i2-1];
-                    dp[i1][i2] = min({add,rep,rem});
+                    int add = 1 + curr[i2-1];
+                    int rem = 1 + prev[i2];   //word1 ka part remove
+                    int rep = 1 + prev[i2-1];
+                    curr[i2] = min({add,rep,rem});
                 }
             }
+            prev = curr;
         }
         
-        return dp[n][m];
+        return prev[m];
     }
 };
