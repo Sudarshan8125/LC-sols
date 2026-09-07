5fcac1badbda486f17cf2e82634cc4b3dda0b277
Sudarshan Rathore <sudarshan08122005@gmail.com>
2026-09-08 00:08:42 +0530

    Time: 13 ms (20.25%), Space: 11.4 MB (5.2%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -2,38 +2,26 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int n = s.size();
-        int l=0,r=0;
-        int maxlen=0;
-        int maxfreq=0;
-        // unordered_map<char,int> mpp;
+        int l = 0;
+        int r = 0;
+        int mxlen = 0;
+        int mxfreq = 0;
 
-        // while(r<n){
-        //     mpp[s[r]]++;
-        //     maxfreq = max(maxfreq,mpp[s[r]]);
-        //     int len = r-l+1;
-        //     if(len-maxfreq>k){
-        //         mpp[s[l]]--;
-        //         l++;
-        //     }
-        //     maxlen = max(maxlen,r-l+1);
-        //     r++;
-        // }
-
-        //USING HASHMAPS//
-        int hash[26] = {0};
+        unordered_map<char,int> hash;
         while(r<n){
+            hash[s[r]]++;
 
-            hash[s[r]-'A']++;
-            maxfreq = max(maxfreq,hash[s[r]-'A']);
+            mxfreq = max(mxfreq,hash[s[r]]);
             int len = r-l+1;
-            if(len-maxfreq>k){
-                hash[s[l]-'A']--;
+            if(len-mxfreq > k ){
+                hash[s[l]]--;
+                if(hash[s[l]] == 0) hash.erase(s[l]);
                 l++;
             }
-
-            maxlen = max(maxlen,r-l+1);
+            
+            mxlen = max(mxlen,r-l+1);
             r++;
         }
-        return maxlen;
+        return mxlen;
     }
 };
